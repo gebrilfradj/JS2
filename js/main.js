88671d473393a56b68dbcdce2546e0d65e8d862e
gebrilfradj <gfradj@ufl.edu>
2023-11-02 22:14:36 -0400

    Lesson 3 done

--- js/main.js
+++ js/main.js
@@ -1,4 +1,3 @@
-console.log("hello world!");
-console.log(typeof "gebril");
-console.log(typeof 42);
-console.log(typeof true);
+const myVariable = "Mathematic";
+
+console.log(myVariable.charAt(5));
