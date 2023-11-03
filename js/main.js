1149bc748e491bd15ce83af4f416a6420f70da1b
gebrilfradj <gfradj@ufl.edu>
2023-11-02 22:24:36 -0400

    Lesson 4 done

--- js/main.js
+++ js/main.js
@@ -1,3 +1,8 @@
-const myVariable = "Mathematic";
+// Numbers
 
-console.log(myVariable.charAt(5));
+//Nan is an acronym for not a number
+
+//The Number.isNaN() method determines wether the passed value is NAn AND its type is Number
+
+//The global isNan() function determines wether a value is NaN or not.
+console.log(Number.isNaN("Dave"));
