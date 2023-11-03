78e9e432b946521e581e7cbe59391f581f396aa5
gebrilfradj <gfradj@ufl.edu>
2023-11-02 22:32:55 -0400

    lesson 5 done

--- js/main.js
+++ js/main.js
@@ -1,8 +1,10 @@
-// Numbers
+// Math Methods and Properties
 
-//Nan is an acronym for not a number
 
-//The Number.isNaN() method determines wether the passed value is NAn AND its type is Number
-
-//The global isNan() function determines wether a value is NaN or not.
-console.log(Number.isNaN("Dave"));
+// from 1 to 10
+console.log(Math.floor(Math.random() * 10)+ 1);
+console.log(Math.floor(Math.random() * 10)+ 1);
+console.log(Math.floor(Math.random() * 10)+ 1);
+console.log(Math.floor(Math.random() * 10)+ 1);
+console.log(Math.floor(Math.random() * 10)+ 1);
+console.log(Math.floor(Math.random() * 10)+ 1);
