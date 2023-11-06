0e4327e26c06b37c2707086e4829e727a67ce8ef
gebrilfradj <gfradj@ufl.edu>
2023-11-06 12:17:23 -0500

    lesson 11 complete

--- js/main.js
+++ js/main.js
@@ -1,11 +1,37 @@
-// user Input
-let name = prompt("Please enter your name.");
-if (name){
-  console.log(name.length);
-  console.log(name.trim().length);
-  console.log(name.trim());
-
+// first interactive game
+let playGame = confirm("Shall we play rock, paper, or scissors?");
+if (playGame){
+  //play
+  let playerChoice = prompt("please enter rock, paper, or scissors.");
+  if (playerChoice){
+    let playerOne = playerChoice.trim().toLowerCase();
+    if (playerOne === "rock" || playerOne === "paper" || playerOne === "scissors"){
+      let computerChoice = Math.floor(Math.random()*3 +1);
+      let computer = computerChoice === 1 ? "rock"
+        : computerChoice === 2 ? "paper"
+        : "scissors";
+      let result =
+      playerOne === computer
+        ? "tie game!"
+        : playerOne === "rock" && computer === "paper"
+        ? 'playerOne: ${playerOne} \nComputer: ${computer} \nComputer wins!'
+        : playerOne === "paper" && computer === "scissors"
+        ? 'playerOne: ${playerOne}\nComputer: ${computer} \nComputer wins!'
+        : playerOne === "scissors" && computer === "rock"
+        ? 'playerOne: ${playerOne}\nComputer: ${computer} \nComputer wins!'
+        :  'playerOne: ${playerOne}\nComputer: ${computer} \nplayerOne wins!';
+        alert(result);
+        let playAgain = confirm("play again?");
+        playAgain ? location.reload() : alert("ok, thanks for playing.");
+    }
+    else{
+      alert("You didn't enter rock, paper, or scissors.");
+    }
+  }
+  else{
+    alert("I guess you changed your mind. Maybe next time.");
+  }
 }
 else{
-  console.log("You didn't enter your name.")
+  alert("ok, maybe next time.");
 }
