135e3da3e92e4632d1e2397fd7ccbaa8eb5dd262
gebrilfradj <gfradj@ufl.edu>
2023-11-06 21:20:17 -0500

    lesson 17 done

--- js/main.js
+++ js/main.js
@@ -1,102 +1,15 @@
-// rock, paper, scissors refactored with Functions
-const initGame = () => {
-  const startGame = confirm("Shall we play rock, paper, or scissors?");
-  startGame ? playGame() : alert("Ok, maybe next time.");
+// objects
+
+const band = {
+  vocals: "Robert Plant",
+  guitar: "Jimmy Page",
+  bass: "John Paul Jones",
+  drums: "John Bonham"
 };
 
-// game flow function
-const playGame = () => {
-  while (true) {
-    let playerChoice = getPlayerChoice();
-    playerChoice = formatPlayerChoice(playerChoice);
-    if (playerChoice === "") {
-      invalidChoice();
-      continue;
-    }
-    if (!playerChoice) {
-      decidedNotToPlay();
-      break;
-    }
-    playerChoice = evaluatePlayerChoice(playerChoice);
-    if (!playerChoice) {
-      invalidChoice();
-      continue;
-    }
-    const computerChoice = getComputerChoice();
-    const result = determineWinner(playerChoice, computerChoice);
-    displayResult(result);
-    if (askToPlayAgain()) {
-      continue;
-    } else {
-      thanksForPlaying();
-      break;
-    }
-  }
-};
+//destructuring objects
 
-const getPlayerChoice = () => {
-  return prompt("Please enter rock, paper, or scissors.");
-};
-
-const formatPlayerChoice = (playerChoice) => {
-  if (playerChoice || playerChoice === "") {
-    return playerChoice.trim().toLowerCase();
-  } else {
-    return false;
-  }
-};
-
-const decidedNotToPlay = () => {
-  alert("I guess you changed your mind. Maybe next time.");
-};
-
-const evaluatePlayerChoice = (playerChoice) => {
-  if (
-    playerChoice === "rock" ||
-    playerChoice === "paper" ||
-    playerChoice === "scissors"
-  ) {
-    return playerChoice;
-  } else {
-    return false;
-  }
-};
-
-const invalidChoice = () => {
-  alert("You didn't enter rock, paper, or scissors.");
-};
-
-const getComputerChoice = () => {
-  const randomNumber = Math.floor(Math.random() * 3);
-  const rpsArray = ["rock", "paper", "scissors"];
-  return rpsArray[randomNumber];
-};
-
-const determineWinner = (player, computer) => {
-  const winner =
-    player === computer
-      ? "Tie game!"
-      : player === "rock" && computer === "paper"
-      ? `playerOne: ${player}\nComputer: ${computer}\nComputer wins!`
-      : player === "paper" && computer === "scissors"
-      ? `playerOne: ${player}\nComputer: ${computer}\nComputer wins!`
-      : player === "scissors" && computer === "rock"
-      ? `playerOne: ${player}\nComputer: ${computer}\nComputer wins!`
-      : `playerOne: ${player}\nComputer: ${computer}\nplayerOne wins!`;
-
-  return winner;
-};
-
-const displayResult = (result) => {
-  alert(result);
-};
-
-const askToPlayAgain = () => {
-  return confirm("Play Again?");
-};
-
-const thanksForPlaying = () => {
-  alert("Ok, thanks for playing.");
-};
-
-initGame();
+function sings({vocals}){
+  return '${vocals} sings!';
+}
+console.log(sings(band));
