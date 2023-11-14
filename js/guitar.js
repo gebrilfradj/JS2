b3fd52a0bfd1e554f9ba18ab950efc0a2d7ab134
gebrilfradj <gfradj@ufl.edu>
2023-11-14 17:23:51 -0500

    lesson 24 done

---
+++ js/guitar.js
@@ -0,0 +1,11 @@
+export function playGuitar(){
+  return "Playing guiter!";
+};
+
+export const shredding = () => {
+  return "Shredding some licks!";
+};
+
+export const plucking = () => {
+  return "plucking the strings...";
+};
--- js/main.js
+++ js/main.js
@@ -1,14 +1,6 @@
-const myarray = ["eat", "sleep", "code"];
-const myObject = {
-  name: "dave",
-  hobbies: ["eat", "sleep", "code"],
-  logName: function() {
-    console.log(this.name);
-  }
-};
+//modules 
+import * as Guitars from "./guitars.js"
 
-localStorage.setItem("myLocalStore", JSON.stringify(myarray));
-const storeLength = localStorage.length;
-const myLocalData = JSON.parse(localStorage.getItem("myLocalStore"));
-
-console.log(storeLength);
+console.log(Guitars.playGuitar());
+console.log(Guitars.shredding());
+console.log(Guitars.plucking());
