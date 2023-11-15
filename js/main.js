9754c0a5388e08888ef5590b0f218b04b9348290
gebrilfradj <gfradj@ufl.edu>
2023-11-14 20:37:53 -0500

    lesson 25 done

--- js/main.js
+++ js/main.js
@@ -1,6 +1,24 @@
-//modules 
-import * as Guitars from "./guitars.js"
+// higher order funtions
 
-console.log(Guitars.playGuitar());
-console.log(Guitars.shredding());
-console.log(Guitars.plucking());
+import {posts } from "./posts.js";
+posts.forEach((post) => {
+  console.log(post);
+});
+console.clear();
+
+const filteredPosts = posts.filter((post) => {
+  return post.userId === 1;
+});
+
+console.log(filteredPosts);
+
+const mappedPosts = filteredPosts.map((post)=> {
+  return post.id * 10;
+});
+console.log(mappedPosts);
+
+const reducedPostsValue = mappedPosts.reduce((sum,post) => {
+  return sum + post;
+});
+
+console.log(reducedPostsValue);
